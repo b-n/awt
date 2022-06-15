a0e4324c25029ded502fd5bad30d028348fe4fbd
Ben Naylor <b.naylor@catawiki.nl>
2022-06-15 11:43:09 +0200

    metrics instead of items

--- src/contact.rs
+++ src/contact.rs
@@ -1,9 +1,25 @@
 use std::cmp::Ordering;
 
+use crate::Agent;
+
 // A contact has the following:
 // - Expected handle time (100% which can be buffed up or down by agent stats)
 // - Expected After handle time (100% which can be buffed up or down by agent stats)
 // - Required training to answer the call
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Metrics {
+    wait: Option<usize>,
+    talk: Option<usize>,
+    after: Option<usize>,
+    result: Option<ContactResult>,
+}
+
+impl Metrics {
+    pub fn new() -> Self {
+        Metrics { wait: None, talk: None, after: None, result: None }
+    }
+}
 
 pub enum ContactType {
     Call,
@@ -23,42 +39,58 @@ pub enum ContactResult {
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Contact {
     skill_preference: Option<usize>,
-    expected_talk_time: usize,
-    expected_acw: usize,
+    base_talk_time: usize,
+    base_acw: usize,
+    time_to_abandon: usize,
     nps: Option<u8>,
-    start: Option<usize>,
+    start: usize,
     end: Option<usize>,
-    result: Option<ContactResult>,
+    metrics: Metrics,
 }
 
 impl Contact {
-    pub fn new() -> Self {
+    pub fn new(start: usize, time_to_abandon: usize) -> Self {
         Contact {
             skill_preference: None,
-            expected_talk_time: 300,
-            expected_acw: 300,
+            base_talk_time: 300,
+            base_acw: 300,
             nps: None,
-            start: None,
+            start,
+            time_to_abandon,
             end: None,
-            result: None,
+            metrics: Metrics::new(),
         }
     }
 
-    pub fn start_contact(&mut self, ticks: usize) {
-        self.start = Some(ticks)
-    } 
-
-    pub fn end_contact(&mut self, ticks: usize) {
-        self.end = Some(ticks)
+    pub fn is_waiting(&self) -> bool {
+        self.metrics.result.is_none()
     }
 
-    pub fn call_length(&mut self) -> Option<usize> {
-        match (self.start, self.end) {
-            (None, _) => None,
-            (_, None) => None,
-            (Some(start), Some(end)) => Some(end - start),
+    /// Tick this contact based on a simulation time
+    ///
+    /// Contact will abandon if not answered in time
+    pub fn tick(&mut self, current_tick: usize) {
+        if current_tick < self.start {
+            panic!("Cannot tick in the past for an already started call");
+        }
+
+        if self.start + self.time_to_abandon < current_tick {
+            self.metrics.result = Some(ContactResult::ABANDONED);
+            self.end = Some(current_tick);
         }
     }
+
+    /// Answer this contact with the provided agent
+    pub fn answer(&mut self, ticks: usize, _: Agent) {
+        let start = self.start;
+        if ticks < start {
+            panic!("Contact cannot be picked up before being started");
+        }
+        self.metrics.wait = Some(ticks - start);
+        self.metrics.talk = Some(self.base_talk_time);
+        self.metrics.after = Some(self.base_acw);
+        self.metrics.result = Some(ContactResult::ANSWERED);
+     }
 }
 
 impl Ord for Contact {
@@ -82,23 +114,12 @@ impl ContactQueue {
         Self { queue: vec![] }
     }
 
-    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut Contact> {
+    pub fn push(&mut self, contact: Contact) {
+        self.queue.push(contact);
+    }
+
+    pub fn waiting(&mut self) -> impl Iterator<Item = &mut Contact> {
         self.queue.iter_mut()
-            .filter(|contact| contact.result == None)
+            .filter(|contact| contact.is_waiting())
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
